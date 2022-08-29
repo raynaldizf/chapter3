7d3ef3c49ef39352fdef1c6d49bb122e3cd28a3e
surezluvy <raynaldizoel@gmail.com>
2022-08-29 20:20:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chapter3/HomeActivity.java
@@ -0,0 +1,6 @@
+package com.example.chapter3;
+
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/chapter3/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.chapter3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnKlik3 : Button
+    lateinit var textHasil2 : TextView
+    lateinit var editNama : EditText
+    lateinit var buttonSimpan : Button
+    lateinit var textNama : TextView
+    lateinit var btnHome : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnKlik = findViewById(R.id.buttonMain) as Button
+        var textHasil = findViewById(R.id.helloWorldMain) as TextView
+        var btnKlik2 = findViewById(R.id.buttonMain2) as Button
+        textHasil2 = findViewById(R.id.txthasil2)
+        btnKlik3 = findViewById(R.id.buttonMain3)
+
+        btnKlik.setOnClickListener{
+            textHasil.text = "Halo, Terimakasih sudah klik button 1"
+        }
+
+        btnKlik2.setOnClickListener{
+            textHasil.text="Ini button klik 2"
+        }
+        setOnclick()
+        saveNama()
+        intentkeHome()
+
+    }
+    fun setOnclick(){
+        textHasil2 = findViewById(R.id.txthasil2)
+        btnKlik3.setOnClickListener{
+            textHasil2.text="Ini adalah button klik 3"
+        }
+    }
+
+    fun saveNama(){
+        editNama =findViewById(R.id.inputNama)
+        buttonSimpan = findViewById(R.id.btnSimpan)
+        textNama = findViewById(R.id.textNama)
+
+        buttonSimpan.setOnClickListener{
+            var nama = editNama.text.toString()
+            textNama.text = nama
+        }
+    }
+    fun intentkeHome(){
+        btnHome = findViewById(R.id.pindahKeHome)
+//        Intent : perpindahan dari 1 activity ke activity lainnya
+        btnHome.setOnClickListener{
+            var pindah = Intent(this,HomeActivity::class.java)
+            startActivity(pindah)
+        }
+    }
+}
